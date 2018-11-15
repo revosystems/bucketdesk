e0347a4ab122f5e223f338acddf5ff911da015d0
Jordi Puigdellívol <jordi@gloobus.net>
2018-11-15 18:11:11 +0100

    styles updated

--- app/ThrustHelpers/Fields/ResolveField.php
+++ app/ThrustHelpers/Fields/ResolveField.php
@@ -8,11 +8,12 @@ class ResolveField extends Link
 {
     public function __construct()
     {
+        $this->classes = 'button secondary shadow-outer-3';
         $this->route = 'issues.resolve';
         $this->displayCallback = function($object){
             if ($object->status < \App\Issue::STATUS_RESOLVED) {
 //                return "<button><i class='fa fa-wrench'></i> RESOLVE</button>";
-                return "<button class='secondary shadow-outer-3'>RESOLVE</button>";
+                return "RESOLVE";
             }
             return "";
         };
