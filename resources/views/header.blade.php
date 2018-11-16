1c0870430fd9837ff1c4418a7f513bcc642dfb93
Jordi Puigdellívol <jordi@gloobus.net>
2018-11-16 11:13:03 +0100

    Added trello to header

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -4,6 +4,7 @@
         <div class="link inline text-center @if (Route::current()->getName() == 'thrust.index') active @endif "><a href="{{route('thrust.index', 'issues')}}">All</a></div>
         <div class="link inline text-center @if (Route::current()->getName() == 'my.issues.current') active @endif "><a href="{{route('my.issues.current')}}">My Current Work</a></div>
         <div class="link inline text-center @if (Route::current()->getName() == 'my.issues.all') active @endif "><a href="{{route('my.issues.all')}}">All My Work</a></div>
+        <div class="link inline text-center @if (Route::current()->getName() == 'trello') active @endif "><a href="{{route('trello')}}">Trello</a></div>
     </div>
     <div class="float-right white font-weight-normal mr2 mt3">
         {{--<form action="{{url('logout')}}" method="post">--}}
